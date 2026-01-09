e1da5ff8df3970f646686629f067518e4cb28220
Binh Minh <bmmit.work@gmail.com>
2026-01-09 09:44:12 +0700

    fix: wrong font-size from injected stylesheet for blockquote

--- apps/web/src/components/note-view/QuoteView.tsx
+++ apps/web/src/components/note-view/QuoteView.tsx
@@ -1,16 +1,21 @@
 import { Container, Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { Blockquote } from "@/components/ui/blockquote";
-import { getRandomQuote } from "@/utils";
+import { getRandomQuote, type Quote } from "@/utils";
 
 export default function QuoteView() {
-  const quote = getRandomQuote();
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  useEffect(() => {
+    getRandomQuote().then(setQuote);
+  }, []);
 
   return (
     <Flex direction="column" w="full" h="full">
       <Container maxW="breakpoint-md" flex="1" padding="4" mx="auto">
         <Flex h="full">
-          <Blockquote cite={quote.author} my="auto" mx="auto">
-            {quote.text}
+          <Blockquote cite={quote?.author} my="auto" mx="auto">
+            {quote?.text}
           </Blockquote>
         </Flex>
       </Container>
--- apps/web/src/utils/index.ts
+++ apps/web/src/utils/index.ts
@@ -1,12 +1,10 @@
-import quotes from "@/../public/qoutes.json";
-
-const QUOTES_COUNT = quotes.length;
-
 export type Quote = {
   author: string;
   text: string;
 };
 
-export function getRandomQuote(): Quote {
-  return quotes[Math.floor(Math.random() * QUOTES_COUNT)];
+export async function getRandomQuote(): Promise<Quote> {
+  const quotes = (await import("../../public/qoutes.json")).default;
+  const quotes_len = quotes.length;
+  return quotes[Math.floor(Math.random() * quotes_len)];
 }
